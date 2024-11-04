0c1312311058c81a85fcc656066210bb28b2876d
Ana V <138679587+anavoronkova@users.noreply.github.com>
2024-11-04 11:08:07 +0100

    Add files via upload

---
+++ duckhunt.hpp
@@ -0,0 +1,22 @@
+#ifndef DUCKHUNT_HPP
+#define DUCKHUNT_HPP
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <cstdlib>  
+
+#define GREEN "\033[1;32m"
+#define MAGENTA "\033[1;35m"
+#define YELLOW "\033[1;33m"
+#define RESET "\033[0m"
+
+std::vector<std::string> create4c();
+std::vector<std::string> create4b();
+std::vector<std::string> create3a();
+std::vector<std::string> create4a();
+std::vector<std::string> create1og();
+
+#endif
---
+++ references.cpp
@@ -0,0 +1,122 @@
+#include "duckhunt.hpp"
+
+std::vector<std::string> create4b() {
+    std::vector<std::string> reference;
+    reference.push_back("111100111111");
+    reference.push_back("1CC0000CCCC1");
+    reference.push_back("1CC0000CCCC1");
+    reference.push_back("1CC0000CCCC1");
+    reference.push_back("1CC0000CCCC1");
+    reference.push_back("1CC0000CCCC1");
+    reference.push_back("1CC0000CCCC1");
+    reference.push_back("1CC0000CCCC1");
+    reference.push_back("1CC0000CCCC1");
+    reference.push_back("1CC0000CCCC1");
+    reference.push_back("1CC0000CCCC1");
+    reference.push_back("1CC0000CCCC1");
+    reference.push_back("1CC000000001");
+    reference.push_back("111100110011");
+    reference.push_back("       10001");
+    reference.push_back("       10001");
+    reference.push_back("       10001");
+    reference.push_back("       11111");
+    return reference;
+}
+
+std::vector<std::string> create4c() {
+    std::vector<std::string> reference;
+    reference.push_back("1111111");
+    reference.push_back("1000001");
+    reference.push_back("1000001");
+    reference.push_back("1000001");
+    reference.push_back("1110001");
+    reference.push_back("100000000001");
+    reference.push_back("1CC0000CCCC1");
+    reference.push_back("1CC0000CCCC1");
+    reference.push_back("1CC0000CCCC1");
+    reference.push_back("1CC0000CCCC1");
+    reference.push_back("1CC0000CCCC1");
+    reference.push_back("1CC0000CCCC1");
+    reference.push_back("111100111111");
+    return reference;
+}
+
+std::vector<std::string> create4a() {
+    std::vector<std::string> map;
+
+    map.push_back("111111");
+    map.push_back("1CCC01");
+    map.push_back("1CCC01");
+    map.push_back("1CCC01");
+    map.push_back("1CCC01");
+    map.push_back("1CCC01001111111");
+    map.push_back("1CCCC000CCCCCC1");
+    map.push_back("1CCCC000CCCCCC1");
+    map.push_back("1CCCC000CCCCCC1");
+    map.push_back("1CCCC000CCCCCC1");
+    map.push_back("1CCCC000CCCCCC1");
+    map.push_back("1CCCC000CCCCCC1");
+    map.push_back("1CCCC000CCCCCC1");
+    map.push_back("1CCCC000CCCCCC1");
+    map.push_back("1CCCC0000000001");
+    map.push_back("100000011111111");
+    map.push_back("111111111111111");
+
+    return map;
+}
+
+std::vector<std::string> create1og() {
+    std::vector<std::string> map;
+
+    map.push_back("        111111111");
+    map.push_back("        100100001");
+    map.push_back("        10000CCC1");
+    map.push_back("        00010CCC1");
+    map.push_back("        100111111");
+    map.push_back("        100100001");
+    map.push_back("        100000001");
+    map.push_back("        100100001");
+    map.push_back("        100100001");
+    map.push_back("11111111100100001");
+    map.push_back("10000000000100001");
+    map.push_back("10000000000000001");
+    map.push_back("10000000000100001");
+    map.push_back("10000000000100001");
+    map.push_back("11111111100111111");
+    map.push_back("        1001");
+    map.push_back("        1001");
+    map.push_back("        100111111");
+    map.push_back("        100100001");
+    map.push_back("        10010CCC1");
+    map.push_back("        10000CCC1");
+    map.push_back("        100111111");
+    map.push_back("        111111111");
+
+    return map;
+}
+
+std::vector<std::string> create3a() {
+    std::vector<std::string> map;
+
+    map.push_back("111111111111111");
+    map.push_back("101111111100001");
+    map.push_back("101111111100001");
+    map.push_back("101111111100001");
+    map.push_back("1000C00000C00C1");
+    map.push_back("10CC0000000CC01");
+    map.push_back("1C00C00000C00C1");
+    map.push_back("100000000000001");
+    map.push_back("100C0C00000C0C1");
+    map.push_back("10C0C0000CC0C01");
+    map.push_back("1C00C00000C0C01");
+    map.push_back("100C0C00000C0C1");
+    map.push_back("100000000000001");
+    map.push_back("111110001111111");
+    map.push_back("1CCCC000CCCCCC1");
+    map.push_back("1CCCC000CCCCCC1");
+    map.push_back("1CCCC000CCCCCC1");
+    map.push_back("1CCC000000CCCC1");
+    map.push_back("111111001111111");
+
+    return map;
+}
